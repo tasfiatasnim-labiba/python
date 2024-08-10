dd29a4aa1e1bec953b6d998e08d3e2e333ce6398
Tasfia Tasnim Labiba <136629763+tasfiatasnim-labiba@users.noreply.github.com>
2024-08-11 00:51:53 +0600

    Delete python/forloop.py

--- python/forloop.py
+++
@@ -1,9 +0,0 @@
-#Print the numbers from 1-20 using for loop.
-
-#Code:
-for i in range(1,21):
-	
-    if (i%2)==0:
-        print(i, "=> The number is even")
-    else:
-	print(i, "=> The number is odd")
